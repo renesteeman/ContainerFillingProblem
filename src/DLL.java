e7caf5466a9a0bfed38275cf4f6886f376ade1f7
Samuele <s.torregrossa@student.maastrichtuniversity.nl>
2019-10-09 12:17:14 +0200

    New branch with AlgoithmX

---
+++ src/DLL.java
@@ -0,0 +1,15 @@
+public class DLL {
+	Node head; // head of list
+
+	/* Doubly Linked list Node*/
+	class Node {
+		int data;
+		Node up;
+		Node down;
+		Node right;
+		Node left;
+
+		// Constructor to create a new node
+		Node(int d) { data = d; }
+	}
+}
--- src/Search.java
+++ src/Search.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Search
 {
-    public static final int horizontalGridSize = 5;
+    public static final int horizontalGridSize = 10;
     public static final int verticalGridSize = 6;
 
-    public static final char[] input = { 'W', 'Y', 'I', 'T', 'Z', 'L'};
+    public static final char[] input = {'P','X','F','V','W','Y','I','T','Z','U','N','L'};
 
     // Static UI class to display the board
     public static UI ui = new UI(horizontalGridSize, verticalGridSize, 50);
@@ -15,20 +17,10 @@ public class Search
     {
         // Initialize an empty board
         int[][] field = new int[horizontalGridSize][verticalGridSize];
-
-        for(int i = 0; i < field.length; i++)
-        {
-            for(int j = 0; j < field[i].length; j++)
-            {
-                // -1 in the state matrix corresponds to empty square
-                // Any positive number identifies the ID of the pentomino
-            	field[i][j] = -1;
-            }
-        }
-        //Start brute force
-        bruteForce(field);
+		wipeField(field);
+		algorithmX(buildMatrix(field));
     }
-
+    //takes the pentomino character and outputs the unique integer ID for it
     private static int characterToID(char character) {
     	int pentID = -1;
     	if (character == 'X') {
@@ -136,8 +128,9 @@ public class Search
 
 
     // Adds a pentomino to the position on the field (overriding current board at that position)
-    public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y)
-    {
+    public static int[][] addPiece(int[][] field, int[][] piece, int pieceID, int x, int y) {
+		int[][] matrix = new int[5][2];
+		int n=0;
         for(int i = 0; i < piece.length; i++) // loop over x position of pentomino
         {
             for (int j = 0; j < piece[i].length; j++) // loop over y position of pentomino
@@ -146,11 +139,124 @@ public class Search
                 {
                     // Add the ID of the pentomino to the board if the pentomino occupies this square
                     field[x + i][y + j] = pieceID;
+                    matrix[n][0]=x + i;
+					matrix[n][1]=y + j;
+                    n++;
                 }
             }
         }
+
+		return matrix;
     }
 
+    //takes the possibilities matrix and outputs a solution for that matrix
+	public static ArrayList<Integer> algorithmX(ArrayList<ArrayList<Integer>> matrix){
+		int minC=1000000;
+		int sumC=0;
+		int indC=0;
+		int n=0;
+		ArrayList<Integer> sol=new ArrayList<Integer>();
+
+		if(matrix.get(0).size()==0){
+			return sol;
+		}
+		for (int i = 0; i < matrix.get(0).size(); i++) {
+			n=0;
+			for (int j = 0; j < matrix.size(); j++) {
+				sumC+=matrix.get(j).get(i);
+				n++;
+			}
+			if(sumC<minC){
+				minC=sumC;
+				indC=i;
+				System.out.println(sumC+", "+indC);
+			}
+			sumC=0;
+		}
+
+		for (int i = 0; i < matrix.size(); i++) {
+			if(matrix.get(i).get(indC)==1){
+				sol=new ArrayList<Integer>();
+			}
+		}
+		return sol;
+	}
+
+	//sets all squares in the field to -1
+	public static void wipeField(int[][] field){
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				field[i][j] = -1;
+			}
+		}
+	}
+
+	//returns a matrix with all possible positions for all pentominoes in the grid
+	public static ArrayList<ArrayList<Integer>> buildMatrix(int[][] field) {
+		int[][] matrix = new int[10000][(horizontalGridSize*verticalGridSize)+12];
+		int[][] onePos;
+		int pentID = 0;
+		int mutation = 0;
+		int[][] pieceToPlace = PentominoDatabase.data[pentID][mutation];
+		int x, y = 0;
+		int row = 0;
+		int n=0;
+		//for every pentomino
+		for (int i = 0; i < input.length; i++) {
+			pentID = characterToID(input[i]);
+			//for every mutation
+			for (int j = 0; j < PentominoDatabase.data[pentID].length; j++) {
+				mutation = j;
+				pieceToPlace = PentominoDatabase.data[pentID][mutation];
+				for (int k = 0; k < field.length; k++) {
+					for (int l = 0; l < field[k].length; l++) {
+						x = k;
+						y = l;
+						if (horizontalGridSize < pieceToPlace.length || x > horizontalGridSize - pieceToPlace.length) {
+							//this particular rotation of the piece is too long for the field
+							x = -1;
+						} else if (horizontalGridSize == pieceToPlace.length) {
+							//this particular rotation of the piece fits perfectly into the width of the field
+							x = 0;
+						}
+
+						if (verticalGridSize < pieceToPlace[0].length || y > verticalGridSize - pieceToPlace[0].length) {
+							//this particular rotation of the piece is too high for the field
+							y = -1;
+						} else if (verticalGridSize == pieceToPlace[0].length) {
+							//this particular rotation of the piece fits perfectly into the height of the field
+							y = 0;
+						}
+
+						//If there is a possibility to place the piece on the field, do it
+						if (x >= 0 && y >= 0) {
+							onePos = addPiece(field, pieceToPlace, pentID, x, y);
+							for (int w = 0; w < onePos.length; w++) {
+								matrix[row][12 + onePos[w][0] + (onePos[w][1] * horizontalGridSize)] = 1;
+							}
+							matrix[row][pentID] = 1;
+							wipeField(field);
+							row++;
+						}
+					}
+				}
+			}
+		}
+
+		Integer[][] matrix1=new Integer[matrix.length][matrix[0].length];
+		for (int k = 0; k < matrix.length; k++) {
+			for (int l = 0; l < matrix[k].length; l++) {
+				matrix1[k][l]=matrix[k][l];
+			}
+		}
+
+		ArrayList<ArrayList<Integer>> list= new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < row; i++) {
+			ArrayList<Integer> arl = new ArrayList<Integer>(Arrays.asList(matrix1[i]));
+			list.add(arl);
+		}
+		return list;
+	}
 
     // Main function. Needs to be executed to start the brute force algorithm
     public static void main(String[] args)
